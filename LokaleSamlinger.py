b9a8214de7942eeae8728fd40476ead479f62542
BalduinLandolt <balduin.landolt@unibas.ch>
2019-08-20 12:15:36 +0200

    Feature: find images

--- LokaleSamlinger.py
+++ LokaleSamlinger.py
@@ -8,9 +8,37 @@ Entry on Digitale Samlinger, (e.g. `7465`).
 """
 
 import sys
-from datetime import time
+import time
 from bs4 import BeautifulSoup
 from requests import get
+
+
+def get_image_urls_from_thumbnails(url):
+    res = []
+
+    response = get(url)
+    bs = BeautifulSoup(response.text, 'lxml')
+    table = bs.find('table', attrs={'class':'ImageTable'})
+    imgs = table.find_all('img')
+    srcs = []
+    for img in imgs:
+        srcs.append('http://digitalesamlinger.hum.ku.dk'+img['src'])
+    return res
+
+
+def get_image_urls(urls):
+    res = []
+    for url in urls:
+        res.extend(get_image_urls_from_thumbnails(url))
+        # TODO: remove
+        if len(res)>2:
+            return res
+        time.sleep(2)
+    return res
+
+
+def save_image(id, url):
+    pass
 
 
 def run_harvester(entry_id):
@@ -19,6 +47,9 @@ def run_harvester(entry_id):
     sub_urls = get_sub_URLs(base_url)
     print(sub_urls)
     time.sleep(2)
+    image_urls = get_image_urls(sub_urls);
+    for i, img in enumerate(image_urls):
+        save_image(i, img)
     # TODO: Do stuff.
 
 
@@ -37,7 +68,15 @@ def get_sub_URLs(base):
     # remove previous and next manuscript
     hrefs.pop(0);
     hrefs.pop(0);
-    return hrefs
+    # put the last one in the beginning
+    hrefs.insert(0, hrefs.pop(len(hrefs)-1))
+    # TODO: not every manuscript has inverted order
+    res = []
+    for ref in hrefs:
+        res.append(ref)
+        res.append(ref+'/2')
+        # TODO: not every manuscript has multiple pages with thumbnails
+    return res
 
 
 if __name__ == "__main__":
